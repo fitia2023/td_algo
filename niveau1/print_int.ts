ae4b71ae70570598df2346d04bc395e431ec56d2
fitia <fitiandrianarisoa@gmail.com>
2024-10-10 13:10:10 +0200

    print_int

---
+++ niveau1/print_int.ts
@@ -0,0 +1,20 @@
+/**************************************************
+*                                                 *
+*        Fonction qui affiche 1 a n               * 
+*                                                 *
+***************************************************/
+
+function print_int(n:number) {
+    
+    for (let index = 0; index < n; index++) {
+        
+        console.log(index+1)
+        
+    }
+
+}
+
+
+print_int(4)
+
+
