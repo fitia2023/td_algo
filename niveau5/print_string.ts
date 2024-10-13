5a8c1d68c1407a983e75ddd0f3799dbb8b9f0d9c
fitia <fitiandrianarisoa@gmail.com>
2024-10-13 20:18:12 +0200

    print string

---
+++ niveau5/print_string.ts
@@ -0,0 +1,17 @@
+/*****************************************************
+*                                                    *
+*           Fonction de print string                 * 
+*                                                    *
+******************************************************/ 
+ 
+
+function print_string(word:string){  
+    for (let i = 0; i < word.length; i++) {
+        console.log(word[i])
+    }
+}
+
+// Input utilisateur
+print_string("Bonjour")
+
+
